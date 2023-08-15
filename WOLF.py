fc30ba2bdff196d51a4176fb6cbb4b43e30eecc2
UNIL-3W <142313652+UNIL-3W@users.noreply.github.com>
2023-08-15 18:12:40 +0600

    Update WOLF.py

--- WOLF.py
+++ WOLF.py
@@ -3,6 +3,6 @@ import os,platform,time
 bitt=platform.architecture()[0]
  
 if bitt=="32bit":
-    import code32.Main
-if bitt=="64bit":
-    import code64.Main
+    import code32
+elif bitt=="64bit":
+    import code64
